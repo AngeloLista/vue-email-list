308ae0e2c6bfcad5ed38733f8dadef45189aee07
AngeloLista <abstractplayer@gmail.com>
2022-02-04 17:36:28 +0100

    created getRandomEmails function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 Vue.config.devtools = true;
 
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {console.log(res.data.response);})
+
 
 const root = new Vue ({
     el: '#root',
     data: {
-
-    }
+        emails: [],
+    },
+    methods: {
+        getRandomEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(res => {
+                console.log(res);
+                this.emails.push(res.data.response);
+            })
+        },
+        getRandomEmails(number){
+            for(let i = 0; i < number; i++) {
+                this.getRandomEmail();
+        }
+        }
+    },
+    mounted() {
+        this.getRandomEmails(10);
+    },
+    
 })
