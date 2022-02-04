635efcf6fe3ee1b20aba3001a735b54a5b39990d
AngeloLista <abstractplayer@gmail.com>
2022-02-04 16:27:45 +0100

    getting random email using axios

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 Vue.config.devtools = true;
 
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {console.log(res.data.response);})
+
 const root = new Vue ({
     el: '#root',
     data: {
-        
+
     }
 })
