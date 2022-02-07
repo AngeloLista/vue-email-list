a6312828162a7c7bcd7c4032aa8258adb7038ce9
AngeloLista <abstractplayer@gmail.com>
2022-02-07 17:04:16 +0100

    checking duplicates in getRandomEmail function

--- js/script.js
+++ js/script.js
@@ -6,22 +6,20 @@ const root = new Vue ({
     el: '#root',
     data: {
         emails: [],
+        mailNum: 10
     },
     methods: {
         getRandomEmail() {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(res => {
-                console.log(res);
-                this.emails.push(res.data.response);
+                const email = res.data.response;
+                if (!this.emails.includes(email)) this.emails.push(email);
+                if (this.emails.length < this.mailNum) this.getRandomEmail();
             })
         },
-        getRandomEmails(number){
-            for(let i = 0; i < number; i++) {
-                this.getRandomEmail();
-        }
-        }
+        
     },
     mounted() {
-        this.getRandomEmails(10);
+        this.getRandomEmail(this.mailNum);
     },
     
 })
